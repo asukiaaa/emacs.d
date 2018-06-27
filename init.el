67a7d1a3966cd3bc64340572778039cc06590aad
Asuk Kono <asukiaaa@gmail.com>
2018-06-27 20:05:23 +0900

    Change values of web-mode to apply prettier-js-mode

--- init.el
+++ init.el
@@ -139,6 +139,19 @@
 ;; slime for roswell
 (load (expand-file-name "~/.roswell/helper.el"))
 
+;; web-mode config
+(defun my-web-mode-hook ()
+  "Hooks for Web mode."
+  (setq web-mode-attr-indent-offset nil)
+  (setq web-mode-markup-indent-offset 2)
+  (setq web-mode-css-indent-offset 2)
+  (setq web-mode-code-indent-offset 2)
+  (setq web-mode-sql-indent-offset 2)
+  (setq indent-tabs-mode nil)
+  (setq tab-width 2))
+(add-hook 'web-mode-hook 'my-web-mode-hook)
+(add-hook 'web-mode-hook 'prettier-js-mode)
+
 ;; ctp as web-mode
 (add-to-list 'auto-mode-alist '("\\.ctp$" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.php$" . web-mode))
